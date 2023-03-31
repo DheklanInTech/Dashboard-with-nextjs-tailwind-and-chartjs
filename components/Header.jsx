da5845a4e208133b8a165a0353bc725172f3ba51
declan <declanintech@gmail.com>
2023-03-31 21:16:16 +0200

    commit

---
+++ components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='flex justify-between p-4 pt-4'>
+        <h2>Dashboard</h2>
+        <h3>Welcome back!</h3>
+    </div>
+  )
+}
+
+export default Header
